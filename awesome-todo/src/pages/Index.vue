252f36b6178759b5b3e487fd9299b6bed99eee42
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-07 12:10:07 +0300

    3.14 Events - Keyboard

--- awesome-todo/src/pages/Index.vue
+++ awesome-todo/src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page padding>
-    <input v-model="message" />
+    <input v-model="message" 
+    @keyup.esc="clearMessage" 
+    @keyup.enter="alertMessage"
+    @mouseleave="alertMessage"
+    />
     <button @click="clearMessage">Clear</button>
     <h5>{{ message }}</h5>
   </q-page>
@@ -16,6 +20,10 @@ export default {
   methods: {
     clearMessage() {
       this.message = "";
+    },
+
+    alertMessage() {
+      alert(this.message);
     }
   }
 };
