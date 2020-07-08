e60d32571ed3063825fa1d26824af9b64631cee4
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-08 14:42:20 +0300

    4 - Assignment 2: practice lists & components

---
+++ assignment2/src/components/FoodCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div 
+  	class="card shadow-1">
+  	<img :src="food.image" width="198" height="180">
+  	<div class="card-content">
+  		<h1 class="text-primary">{{ food.name }}</h1>
+  		<p>{{ food.description }}</p>
+  		<p>
+	  		<small>
+	  			<b>Deliciousness:</b>
+	  		</small>
+	  		<b class="text-primary">
+  	  		{{ food.deliciousness }}/5
+	  		</b>
+  		</p>
+  	</div>
+  </div>
+</template>
+
+<script>
+	export default {
+        name:"foodCard",
+		props: ['food']
+	}
+</script>
