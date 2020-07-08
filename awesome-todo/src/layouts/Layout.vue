a5c26997c1e5877baf89c45bccc1d93c9920d657
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-08 16:07:35 +0300

    5.33 Add Page Navigation to Sidebar (Drawer)

--- awesome-todo/src/layouts/Layout.vue
+++ awesome-todo/src/layouts/Layout.vue
@@ -26,17 +26,29 @@
       content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
+        <q-item-label header class="text-grey-8">
+          Navigation
         </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+
+        <q-item to="/" exact clickable>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Todo</q-item-label>
+          </q-item-section>
+          ></q-item
+        >
+
+        <q-item to="/settings" clickable>
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+          ></q-item
+        >
       </q-list>
     </q-drawer>
 
@@ -47,63 +59,13 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-    EssentialLink
-  },
-
-  data () {
+  data() {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
-      ]
-    }
+      leftDrawerOpen: false
+    };
   }
-}
+};
 </script>
--- awesome-todo/src/pages/PageSettings.vue
+++ awesome-todo/src/pages/PageSettings.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page>
+  <q-page padding>
       <p>Todo Settings</p>
   </q-page>
 </template>
