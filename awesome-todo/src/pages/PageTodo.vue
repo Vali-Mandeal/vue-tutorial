8eec2488454864cc18bb9438c463185be11c1bd9
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-14 13:28:45 +0300

    6.44 - Add some Style to the List of Tasks

--- awesome-todo/src/pages/PageTodo.vue
+++ awesome-todo/src/pages/PageTodo.vue
@@ -1,19 +1,24 @@
 <template>
   <q-page class="q-pa-md">
-    <q-list bordered>
+    <q-list 
+    bordered
+    separator>
       <q-item
         v-for="task in tasks"
         :key="task.id"
         @click="task.completed = !task.completed"
         clickable
         v-ripple
+        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
       >
         <q-item-section side top>
           <q-checkbox v-model="task.completed" />
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>{{ task.name }}</q-item-label>
+          <q-item-label
+          :class = "{'text-strikethrough' : task.completed}"
+          >{{ task.name }}</q-item-label>
         </q-item-section>
 
         <q-item-section side>
