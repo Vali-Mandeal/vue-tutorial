e19498c443633aa05b8e2f3b530e47549b94ee28
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-14 12:08:27 +0300

    6.42 - Display a List of Tasks

--- awesome-todo/src/pages/PageTodo.vue
+++ awesome-todo/src/pages/PageTodo.vue
@@ -1,6 +1,24 @@
 <template>
-  <q-page padding>
-   <p>Page Todo</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+
+      <q-item 
+      v-for="task in tasks" 
+      :key = "task.id"
+      @click="task.completed = !task.completed"
+      clickable
+      v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed"/>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+        </q-item>
+
+    </q-list>
+   
   </q-page>
 </template>
 
@@ -8,7 +26,23 @@
 export default {
   data() {
     return {
-     
+     tasks:[
+       {
+         id: 1,
+         name: 'Go to shop',
+         completed: false
+       },
+        {
+         id: 2,
+         name: 'Get banans',
+         completed: false
+       },
+        {
+         id: 3,
+         name: 'Get apples',
+         completed: false
+       }
+     ]
     };
   },
 
