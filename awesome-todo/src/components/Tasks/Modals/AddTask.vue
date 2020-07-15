211a9c5fd67459afdfea78cbbab377a83f63135d
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-15 14:00:23 +0300

    9.60 - Add Task - Customize the Modal

--- awesome-todo/src/components/Tasks/Modals/AddTask.vue
+++ awesome-todo/src/components/Tasks/Modals/AddTask.vue
@@ -1,27 +1,25 @@
 <template>
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Alert</div>
-        </q-card-section>
+  <q-card>
+    <q-card-section class="row">
+      <div class="text-h6">Add Task</div>
+      <q-space/>
+      <q-btn v-close-popup flat color="primary" dense icon="close"/>
+    </q-card-section>
 
-        <q-card-section>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, pariatur
-          alias architecto ipsum repudiandae quisquam fuga ducimus, ullam sit
-          distinctio obcaecati dolor illum cupiditate nihil!
-        </q-card-section>
+    <q-card-section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, pariatur
+      alias architecto ipsum repudiandae quisquam fuga ducimus, ullam sit
+      distinctio obcaecati dolor illum cupiditate nihil!
+    </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" />
-        </q-card-actions>
-      </q-card>
+    <q-card-actions align="right">
+      <q-btn flat label="Save" color="primary" />
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
