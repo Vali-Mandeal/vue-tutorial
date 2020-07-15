2c510d2910f894d7c3d42b19979453869837ef30
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-15 15:38:37 +0300

    9.61 - Add the Fields

--- awesome-todo/src/components/Tasks/Modals/AddTask.vue
+++ awesome-todo/src/components/Tasks/Modals/AddTask.vue
@@ -2,14 +2,38 @@
   <q-card>
     <q-card-section class="row">
       <div class="text-h6">Add Task</div>
-      <q-space/>
-      <q-btn v-close-popup flat color="primary" dense icon="close"/>
+      <q-space />
+      <q-btn v-close-popup flat color="primary" dense icon="close" />
     </q-card-section>
 
     <q-card-section>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, pariatur
-      alias architecto ipsum repudiandae quisquam fuga ducimus, ullam sit
-      distinctio obcaecati dolor illum cupiditate nihil!
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.name" label="Task name" class="col"/>
+      </div>
+
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.dueDate" label="Due date">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy>
+                <q-date v-model="taskToSubmit.dueDate" />
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+      <div class="row q-mb-sm">
+        <q-input outlined v-model="taskToSubmit.dueTime" label="Due time">
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="taskToSubmit.dueTime" />
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
     </q-card-section>
 
     <q-card-actions align="right">
@@ -19,7 +43,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      taskToSubmit: {
+        name: "",
+        dueDate: "",
+        dueTime: "",
+        completed: false
+      }
+    };
+  }
+};
 </script>
 
 <style></style>
