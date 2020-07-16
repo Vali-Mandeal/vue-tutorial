fb8d46273a1f9ff2b615e023ed0a54efbf3eec9a
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-16 10:44:28 +0300

    9.62 - Form Validation & Submission

--- awesome-todo/src/components/Tasks/Modals/AddTask.vue
+++ awesome-todo/src/components/Tasks/Modals/AddTask.vue
@@ -3,42 +3,60 @@
     <q-card-section class="row">
       <div class="text-h6">Add Task</div>
       <q-space />
-      <q-btn v-close-popup flat color="primary" dense icon="close" />
+      <q-btn 
+      v-close-popup 
+      flat 
+      color="primary" 
+      dense 
+      icon="close" />
     </q-card-section>
 
-    <q-card-section>
-      <div class="row q-mb-sm">
-        <q-input outlined v-model="taskToSubmit.name" label="Task name" class="col"/>
-      </div>
+    <q-form @submit.prevent="submitForm">
+      <q-card-section>
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            v-model="taskToSubmit.name"
+            label="Task name"
+            class="col"
+            :rules="[val => !!val || 'Field is required']"
+            ref="name"
+          />
+        </div>
 
-      <div class="row q-mb-sm">
-        <q-input outlined v-model="taskToSubmit.dueDate" label="Due date">
-          <template v-slot:append>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy>
-                <q-date v-model="taskToSubmit.dueDate" />
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-      </div>
+        <div class="row q-mb-sm">
+          <q-input outlined v-model="taskToSubmit.dueDate" label="Due date">
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy>
+                  <q-date v-model="taskToSubmit.dueDate" />
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
 
-      <div class="row q-mb-sm">
-        <q-input outlined v-model="taskToSubmit.dueTime" label="Due time">
-          <template v-slot:append>
-            <q-icon name="access_time" class="cursor-pointer">
-              <q-popup-proxy transition-show="scale" transition-hide="scale">
-                <q-time v-model="taskToSubmit.dueTime" />
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-      </div>
-    </q-card-section>
+        <div class="row q-mb-sm">
+          <q-input outlined v-model="taskToSubmit.dueTime" label="Due time">
+            <template v-slot:append>
+              <q-icon name="access_time" class="cursor-pointer">
+                <q-popup-proxy transition-show="scale" transition-hide="scale">
+                  <q-time v-model="taskToSubmit.dueTime" />
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
+      </q-card-section>
 
-    <q-card-actions align="right">
-      <q-btn flat label="Save" color="primary" />
-    </q-card-actions>
+      <q-card-actions align="right">
+        <q-btn 
+        @click="submitForm" 
+        label="Save" 
+        color="primary" />
+      </q-card-actions>
+    </q-form>
+
   </q-card>
 </template>
 
@@ -53,6 +71,18 @@ export default {
         completed: false
       }
     };
+  },
+  methods: {
+    submitForm() {
+      console.log("bla");
+      if(!this.$refs.name.hasError) {
+          this.submitTask();
+      }
+    },
+
+    submitTask () {
+        console.log('task')   
+    }
   }
 };
 </script>
