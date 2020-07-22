d0c2a7f153799cf67115355120e95945c76cdb35
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-22 10:10:07 +0300

    14.100 - Custom Directive - Select All Text in Input when Clicked

--- awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -1,27 +1,38 @@
 <template>
-    <div class="row q-mb-sm">
-          <q-input
-            autofocus
-            outlined
-            :value="name"
-            @input="$emit('update:name', $event)"
-            label="Task name"
-            class="col"
-            :rules="[val => !!val || 'Field is required']"
-            ref="name"
-            clearable
-          />
-        </div>
+  <div class="row q-mb-sm">
+    <q-input
+      autofocus
+      v-select-all
+      outlined
+      :value="name"
+      @input="$emit('update:name', $event)"
+      label="Task name"
+      class="col"
+      :rules="[val => !!val || 'Field is required']"
+      ref="name"
+      clearable
+    />
+  </div>
 </template>
 
 <script>
 export default {
-props: [
-    "name"
-]
-}
+  props: ["name"],
+
+  directives: {
+    selectAll: {
+      inserted(el) {
+        let input = el.querySelector(".q-field__native");
+        
+        input.addEventListener("focus", () => {
+          if (input.value.length) {
+            input.select();
+          }
+        });
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
