3e7b76d456174b6d0e502d7fd912875526cdf265
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-23 11:06:27 +0300

    15.112 - Add a "Show 12 hour time format" Setting

--- awesome-todo/src/pages/PageSettings.vue
+++ awesome-todo/src/pages/PageSettings.vue
@@ -1,15 +1,28 @@
 <template>
   <q-page padding>
-      <p>Todo Settings</p>
+    <q-list bordered padding>
+      <q-item-label header>Settings</q-item-label>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show 12 hour time format</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-toggle color="blue" v-model="show12HourTimeFormat" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      show12HourTimeFormat: false
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
