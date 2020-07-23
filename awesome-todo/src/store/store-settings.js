72eb4c411332ac3a2ca5eb282c20310957cbb86b
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-23 11:17:17 +0300

    15.114 - Add a Computed Getter & Setter

--- awesome-todo/src/store/store-settings.js
+++ awesome-todo/src/store/store-settings.js
@@ -5,11 +5,15 @@ const state = {
 };
 // not async. Only mutate state instantly
 const mutations = {
-
+  setShow12HourTimeFormat(state, value) {
+    state.settings.show12HourTimeFormat = value;
+  }
 };
 // async, good for data fetching
 const actions = {
- 
+  setShow12HourTimeFormat({ commit }, value) {
+    commit('setShow12HourTimeFormat', value)
+  }
 };
 // get data from the state -> use it in the components
 const getters = {
