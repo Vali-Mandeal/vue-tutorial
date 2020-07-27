9095380166e7fa60e17b4612be84ca9d04f7ebe5
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-07-27 13:34:31 +0300

    16.127 - Register - Add Email/Password Fields & Button

--- awesome-todo/src/components/Auth/Register.vue
+++ awesome-todo/src/components/Auth/Register.vue
@@ -1,14 +1,53 @@
 <template>
-  <q-banner class="bg-grey-3">
-    <template v-slot:avatar>
-      <q-icon name="login" color="primary" />
-    </template>
-    Register to acces your Todos anywhere!
-  </q-banner>
+  <form>
+    <div class="row q-mb-md">
+      <q-banner class="bg-grey-3 col">
+        <template v-slot:avatar>
+          <q-icon name="login" color="primary" />
+        </template>
+        Register to acces your Todos anywhere!
+      </q-banner>
+    </div>
+
+    <div class="row q-mb-md">
+      <q-input
+        v-model="formData.email"
+        label="Email"
+        stack-label
+        :dense="dense"
+        class="col"
+      />
+    </div>
+
+    <div class="row q-mb-md">
+      <q-input
+        v-model="formData.password"
+        type="password"
+        label="Password"
+        stack-label
+        :dense="dense"
+        class="col"
+      />
+    </div>
+
+    <div class="row">
+      <div class="q-space" />
+      <q-btn color="primary" label="Register" />
+    </div>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
 <style></style>
